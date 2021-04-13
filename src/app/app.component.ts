d6508661666cee1e044ddadad654c0b891444069
AdrianPerez550 <201822143@tese.edu.mx>
2021-04-13 15:58:42 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,15 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  title:String ="Contador Basico Evaluacion";
+  title:String ="Contador";
   numero: number= 10;
   base: number =5;
 
 acumular(valor: number){
 this.numero +=  valor;
+}
+desacumular(valor: number){
+this.numero -=  valor;
 }
 
 
